ce63c6707855f8440a4e5eef203e0a4e91f676a8
dzpqzb <dz@wiz.cn>
2013-05-11 16:29:33 +0800

    上传文件成功

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import xmlrpclib
-
-s = xmlrpclib.Server('http://as.wiz.cn/wizas/xmlrpc')
-a = s.accounts.clientLogin({'user_id':'yishuiliunian@gmail.com','password':'654321'})
-print a
-
--- src/xmlrpcServer.py
+++ src/xmlrpcServer.py
@@ -4,10 +4,21 @@ import hashlib
 import uuid
 import pdb
 import sys
+import StringIO
+import zipfile
+import base64
+import datetime
 reload(sys) 
 sys.setdefaultencoding('utf8')
 
 from wizhelper.WizServerUrl import WizServerUrl
+
+def encodebindata(data):
+	out = StringIO.StringIO()
+	bin = StringIO.StringIO(data)
+	base64.encode(bin,out)
+	return out.getvalue()
+
 def addCommonParams(postParams):
 	postParams['client_type']='python_command'
 	postParams['program_type']='normal'
@@ -39,7 +50,34 @@ def md5File(name):
 	return m.hexdigest()
 class WizDocument:
 	def __init__(self):
-		self.guid = uuid.uuid1().hex 
+		self.guid = '%s' % uuid.uuid1()
+		self.data_md5 = ''
+		self.document_category = '/My Notes/'
+		self.document_attachment_count = 0
+		self.document_location = '/My Notes/'
+		self.document_protect = 0
+		self.document_tag_guids = ''
+		self.document_title = 'Python Auto Saved File'
+		self.document_type = 'PythonAutoSavedFile'
+		self.document_zip_md5 = ''
+		self.dt_created = datetime.datetime.today()
+		self.dt_modified = self.dt_created 
+
+	def toServerDictionary(self):
+		dic = {}
+		dic['data_md5'] = self.data_md5
+		dic['document_attachment_count'] = self.document_attachment_count
+		dic['document_category'] = self.document_category
+		dic['document_guid'] = self.guid
+		dic['document_location'] = self.document_location
+		dic['document_protect'] = self.document_protect
+		dic['document_tag_guids'] = self.document_tag_guids
+		dic['document_title'] = self.document_title
+		dic['document_type'] = self.document_type
+		dic['document_zip_md5'] = self.document_zip_md5
+		dic['dt_created'] = xmlrpclib.DateTime(self.dt_created)
+		dic['dt_modified'] = xmlrpclib.DateTime(self.dt_modified)
+		return dic
 
 def addToken(params, token):
 	params['token'] = token
@@ -51,7 +89,9 @@ def uploadFile(filePath):
 		return
 	#
 	document = WizDocument()
-	document.dataMd5 = md5File(filePath)
+	document.data_md5 = md5File(filePath)
+	document.title = 'aaa'
+	document.document_zip_md5 = document.data_md5;
 	#
 	userId = 'yishuiliunian@gmail.com'
 	password = '654321'
@@ -60,7 +100,7 @@ def uploadFile(filePath):
 	kbguid = getKbguid(loginData)
 	kmUrl = loginData['kapi_url']	
 	#
-	kmXmlServer = xmlrpclib.Server(kmUrl)
+	kmXmlServer = xmlrpclib.Server(kmUrl,verbose=True)
 	print kmXmlServer
 	#
 	sizehint = 128*1024
@@ -84,8 +124,9 @@ def uploadFile(filePath):
 		params['data'] = xmlrpclib.Binary(lines)
 		params['obj_type']='document'
 		params['part_md5'] = md5Data(lines) 
+		params['part_size']=len(lines)
 		params['obj_guid'] = document.guid
-		params['obj_md5'] = document.dataMd5
+		params['obj_md5'] = document.data_md5
 		print params
 		addToken(params, token)
 		pdb.set_trace()
@@ -93,5 +134,21 @@ def uploadFile(filePath):
 		###
 		lines = file.read(sizehint)
 		partSN += 1
-	
-uploadFile('a.txt')
+	dic = document.toServerDictionary()
+	addCommonParams(dic)
+	addToken(dic, token)
+	dic['with_document_data']=1
+	kmXmlServer.document.postSimpleData(dic)
+def uploadTxtFile(filePath):
+	zipname = 'temp.ziw'
+	e = os.path.exists(zipname)
+	if e:
+		os.remove(zipname)
+	zipFile = zipfile.ZipFile(zipname,'w')
+	zipFile.write(filePath,'index.html',zipfile.ZIP_DEFLATED)
+	zipFile.close()
+	try:
+		uploadFile(zipname)
+	finally:
+		os.remove(zipname)
+uploadTxtFile('a.txt')
